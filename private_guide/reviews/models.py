7b079387daeca3f911bcb31ee0c7d043db429798
michael-basweti <baswetima@gmail.com>
2019-04-11 14:57:45 +0300

    Add review model

--- private_guide/reviews/models.py
+++ private_guide/reviews/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from authentication.models import User
+from create_guide_profile.models import Profile
 
-# Create your models here.
+
+class Review(models.Model):
+    reviewer = models.ForeignKey(User, related_name="reviewer", on_delete=models.CASCADE)
+    profile_id = models.ForeignKey(Profile, related_name="profile_id", on_delete=models.CASCADE)
+    review = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
