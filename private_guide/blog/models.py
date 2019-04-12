d3a72b0488cdc55f4a66b06b7cc7715442b3b9b9
michael-basweti <baswetima@gmail.com>
2019-04-12 15:15:48 +0300

    add a blog model

--- private_guide/blog/models.py
+++ private_guide/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from authentication.models import User
 
-# Create your models here.
+class Blog(models.Model):
+    user = models.ForeignKey(User, related_name="author", on_delete=models.CASCADE)
+    title = models.CharField(max_length=1000, null=False, blank = False)
+    body = models.TextField(null=False, blank = False)
+    created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
