b86cc79ec7070f95c67018d7bafd091f84e232b7
michael-basweti <baswetima@gmail.com>
2019-04-12 15:47:38 +0300

    Add image field to Blog model

--- private_guide/blog/models.py
+++ private_guide/blog/models.py
@@ -5,5 +5,6 @@ class Blog(models.Model):
     user = models.ForeignKey(User, related_name="author", on_delete=models.CASCADE)
     title = models.CharField(max_length=1000, null=False, blank = False)
     body = models.TextField(null=False, blank = False)
+    image = models.URLField(null=True)
     created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
