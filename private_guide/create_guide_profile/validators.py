0b36b9ed6a83848a051234d3608e3acaa282888d
michael-basweti <baswetima@gmail.com>
2019-05-07 14:51:23 +0300

    increase storage

--- private_guide/create_guide_profile/validators.py
+++ private_guide/create_guide_profile/validators.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 def validate_file_size(value):
     filesize= value.size
     
-    if filesize > 10485760:
-        raise ValidationError("The maximum file size that can be uploaded is 10MB")
+    if filesize > 20971520:
+        raise ValidationError("The maximum file size that can be uploaded is 20MB")
     else:
         return value
